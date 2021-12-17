d15342b21389e1488ff788e1c1a5d036303be79f
Claire <claire.brisbart@edu.devinci.fr>
2021-12-17 14:45:48 +0100

    :sparkles: Add route /missions and show missions list

---
+++ src/Controller/MissionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Mission;
+use App\Repository\MissionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MissionController extends AbstractController
+{
+    /** @var MissionRepository */
+    private $missionRepository;
+
+    public function __construct(EntityManagerInterface $objectManager)
+    {
+        $this->missionRepository = $objectManager->getRepository(Mission::class);
+    }
+
+    // INDEX
+    #[Route('/missions', name: 'missions_list')]
+    public function missionsList(): Response
+    {
+        $missions = $this->missionRepository->findAll();
+
+        return $this->render('mission/index.html.twig', [
+            'controller_name' => 'MissionController',
+            'missions' => $missions
+        ]);
+    }
+}
